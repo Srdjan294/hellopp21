a265396918d06f407d86b071f6e90da314ba5cc4
Srdjan294 <srdjanfilipovic991@gmail.com>
2020-06-15 12:13:12 +0200

    skripte

---
+++ predavac01.edunova.hr_04/predavac01.edunova.hr/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>10. 06. 2020</title>
+</head>
+<body>
+
+<ul>
+<li>
+<a target="_blank" href="ugradeniNizovi.php">Ugrađeni nizovi</a>
+</li>
+<li>
+<a target="_blank" href="operatori.php">Operatori</a>
+</li>
+</ul>
+    
+    <hr />
+    <?php echo $_SERVER['SERVER_ADDR']; ?>
+</body>
+</html>
